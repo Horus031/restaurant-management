39d244aa2069dbb493a56e1fa82d252b353d1d16
Horus031 <vominhnghia1878@gmail.com>
2026-04-30 18:52:00 +0700

    Add http client file for api calling; creating schema validation with zod

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import envConfig from "@/config";
 import Image from "next/image";
 
 export default function Home() {
+  console.log(envConfig.NEXT_PUBLIC_API_ENDPOINT);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
